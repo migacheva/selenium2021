39095b4820309f888562ccb10298ae7df00f40a8
migacheva <anutamig@mail.ru>
2021-06-01 16:00:19 +0300

    Задание 9. Проверить сортировку стран и геозон в админке Сделайте сценарии, которые проверяют сортировку стран и геозон (штатов) в учебном приложении litecart.

---
+++ helpers.py
@@ -0,0 +1,6 @@
+
+def login_admin(driver):
+    driver.get("http://localhost/litecart/admin")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
--- test_start.py
+++ test_start.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from helpers import login_admin
+
 
 @pytest.fixture
 def driver(request):
@@ -72,4 +74,45 @@ def test_check_stickers_lab4(driver):
         assert len(elem.find_elements_by_css_selector('[class^="sticker"]')) == 1, \
             "Для элемента найдено более одного стикера или не найдено вовсе"
 
+
+def test_sorted_countries(driver):
+    """
+    Задание 9. Проверить сортировку стран и геозон в админке
+    Сделайте сценарии, которые проверяют сортировку стран и геозон (штатов) в учебном приложении litecart.
+    1) на странице http://localhost/litecart/admin/?app=countries&doc=countries
+        а) проверить, что страны расположены в алфавитном порядке
+        б) для тех стран, у которых количество зон отлично от нуля -- открыть страницу этой страны и там проверить, что зоны расположены в алфавитном порядке
+    2) на странице http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones
+    зайти в каждую из стран и проверить, что зоны расположены в алфавитном порядке"""
+    login_admin(driver)
+    driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    table_count = driver.find_elements_by_css_selector('[name="countries_form"] tr')
+    list_countries = []
+    for i in range(2, len(table_count) - 1):
+        list_countries.append(driver.find_element_by_css_selector(f'[name="countries_form"] tr:nth-child({i}) > td:nth-child(5)').text)
+        zone = driver.find_element_by_css_selector(f'[name="countries_form"] tr:nth-child({i}) > td:nth-child(6)').text
+        if zone != '0':
+            list_inside = []
+            driver.find_element_by_css_selector(f'[name="countries_form"] tr:nth-child({i}) > td:nth-child(5) > a').click()
+            tab = driver.find_elements_by_css_selector('#table-zones.dataTable tr:not(.header)')
+            for z in range(2, len(tab)+1):
+                list_inside.append(driver.find_element_by_css_selector(f"tr:nth-child({z}) > td:nth-child(3)").text)
+            sort_list_inside = sorted(list_inside)
+            assert sort_list_inside == list_inside, f"Список не отсортирован для {driver.find_element_by_css_selector(f'[name=countries_form] tr:nth-child({i}) > td:nth-child(5)').text}"
+            driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    sorted_list_countries = sorted(list_countries)
+    assert list_countries == sorted_list_countries, "Основной список не отсортирован"
+    # Пункт 2
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+    for i in [2, 3]:
+        zone = driver.find_element_by_css_selector(f'tr:nth-child({i}) > td:nth-child(4)').text
+        if zone != '0':
+            driver.find_element_by_css_selector(f'tr:nth-child({i}) > td:nth-child(3) > a').click()
+            list_zones = []
+            count_z = driver.find_elements_by_css_selector('#table-zones > tbody > tr')
+            for z in range(2, len(count_z)):
+                list_zones.append(driver.find_element_by_css_selector(f'tr:nth-child({z}) > td:nth-child(3) [selected="selected"]').text)
+            sort_list_zones = sorted(list_zones)
+            assert sort_list_zones == list_zones, f"Список не отсортирован"
+            driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
 
